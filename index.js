44895f50d8fa0f9fa268a53a6821d911ba88f1c3
sidm97 <sid.m.974@gmail.com>
2024-01-02 13:24:37 +0000

    added total months and total money code

--- index.js
+++ index.js
@@ -88,25 +88,27 @@ var finances = [
 ];
 
 
+// total number of months = count the number of entries in the array
 
 
+let totalMonths = 0
+
+let total = 0
+
+for (let i = 0; i < finances.length; i++) {
+  const arr = finances[i]
+  total += arr[1]
+}
+
+console.log(total);
+
+console.log(finances.length);
 
 
+for (let i = 0; i < finances.length; i++) {
+let numbers = finances[i][1]
 
-
-
-
-
-// ## Overview
-
-// In this challenge, you'll be using the concepts you've learned to complete the required activity. This activity presents a real-world situation in which your newfound JavaScript skills will come in handy. You are tasked with creating code for analyzing the financial records of a company. You have been provided with a financial dataset in the `starter/index.js` file.
-
-// ## Instructions
-
-// 1. Create a new GitHub repo called `Console-Finances`. Then, clone it to your computer.
-
-// 2. Copy the starter files in your local git repository.
-
+}
 // You have been given a dataset composed of arrays with two fields, Date and Profit/Losses.
 
 // Your task is to write JavaScript code that analyzes the records to calculate each of the following:
